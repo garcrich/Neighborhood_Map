5c22fa6e4573be6d3f252247b075e97b9ec0f36e
Ricky Garcia <garcrich@outlook.com>
2016-05-02 15:26:39 -0600

    fix: removed list. Will build again with KO JS

--- js/map-refactored.js
+++ js/map-refactored.js
@@ -1,38 +1,30 @@
-
-    // So we can talk about the map a bunch before I call it - made global.
-    var map;
-    var infowindow = new google.maps.InfoWindow;
-    var marker,
-      contentString;
-    // Array for marker objects
-      var locations = [
-      {
+  var locations = [{
       "title": "Idaho Central Credit Union - HQ",
       "lat": 42.914059,
       "lng": -112.462819,
       "streetAddress": "4400 Central Way",
-      "website": "https://www.iccu.com/"
+      "website": "https://www.iccu.com/",
       },
       {
       "title": "DL Evans Bank",
       "lat": 42.907624,
       "lng": -112.465672,
       "streetAddress": "4080 Yellowstone Ave",
-      "website": "https://www.dlevans.com/"
+      "website": "https://www.dlevans.com/",
       },
       {
       "title": "Wells Fargo",
       "lat": 42.909370,
       "lng": -112.466802,
       "streetAddress": "4195 Yellowstone Ave",
-      "website": "https://www.wellsfargo.com/"
+      "website": "https://www.wellsfargo.com/",
       },
       {
       "title": "Idaho Central Credit Union - Chubbuck Branch",
       "lat": 42.915882,
       "lng": -112.466823,
       "streetAddress": "4537 Yellowstone Ave",
-      "website": "https://www.iccu.com/"
+      "website": "https://www.iccu.com/",
       },
       {
       "title": "Mountain America Credit Union",
@@ -40,21 +32,24 @@
       "lng": -112.464667,
       "streetAddress": "152 Bullock St",
       "cityAddress": "Washington, DC 20500",
-      "website": "https://www.macu.com"
+      "website": "https://www.macu.com",
+
       },
       {
       "title": "Idaho State University Credit Union",
       "lat": 42.9227200,
       "lng": -112.4658335,
       "streetAddress": "4914 Yellowstone Ave",
-      "website": "https://www.isucu.org/"
+      "website": "https://www.isucu.org/",
+
       },
       {
       "title": "Advantage Plus Credit Union",
       "lat": 42.920881,
       "lng": -112.468284,
       "streetAddress": "150 W Chubbuck Rd",
-      "website": "https://advantagepluscreditunion.com"
+      "website": "https://advantagepluscreditunion.com",
+
       },
       {
       "title": "Pocatello Railroad Federal Credit",
@@ -64,7 +59,21 @@
       "website": "https://railswestcu.org"
       }];
 
-    function makeMarker() {
+var PlaceConstructor = function(dataObj){
+  this.name = ko.observable(dataObj.title);
+  this.lat = ko.observable(dataObj.lat);
+  this.lng = ko.observable(dataObj.lng);
+  // ...etc...
+  // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
+}
+
+var ViewModel = function(){
+  var self = this;
+  // for clarity of scope
+
+  self.arrayOfAllMyLocations = ko.observableArray(locations);
+
+  self.createMarkers = function makeMarker() {
       for(var i = 0; i < locations.length; i++) {
             marker = new google.maps.Marker({
             position: {lat: locations[i].lat, lng: locations[i].lng},
@@ -79,22 +88,21 @@
             }
         })(marker, i));
       }
+    // make the markers, set event listeners, get infowindow content, etc
+    // this may be pieced out into as many smaller functions as you think make sense
   }
 
-  function addButtons() {
-    document.getElementById('btnTerrain').addEventListener('click', function() {
-      map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
-    });
-    document.getElementById('btnSatellite').addEventListener('click', function() {
-      map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-    });
-    document.getElementById('btnRoadmap').addEventListener('click', function() {
-      map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-    });
-    document.getElementById('btnHybrid').addEventListener('click', function() {
-      map.setMapTypeId(google.maps.MapTypeId.HYBRID);
-    })
-  };
+  self.filterLocations = function() {
+    // there are many ways to accomplish this, consider using
+    // http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+    // and
+    // ko.utils.arrayFilter()
+    // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+  }
+
+  // you will probably need to make additional functions to make this code modular and clean
+  // or to add your unique functionality
+}
 
   function initMap() {
     var mapOptions = {
@@ -114,4 +122,6 @@
     makeMarker();
   }
   addButtons();
-  google.maps.event.addDomListener(window, "load", initMap);
+  appendList();
+  google.maps.event.addDomListener(window, "load", initialize);
+
