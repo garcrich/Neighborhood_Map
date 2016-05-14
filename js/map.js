e49ca3f9229c9c38933ba1174cd84610b5a77cd4
Ricky Garcia <garcrich@outlook.com>
2016-05-14 12:08:50 -0600

    fix: changed locations from banks to coffee shops. More reviews are available for coffee shops which will make 3rd party api inclusion easier

--- js/map.js
+++ js/map.js
@@ -4,15 +4,14 @@ var marker;
 
 //revealing module pattern
 var banks = ko.observableArray([
-    //var location = function (koTitle, title,latitude,longitude,streetAddress,website, setVisible)
-    new Location("Idaho Central Credit Union - HQ", "Idaho Central Credit Union - HQ", 42.914059, -112.462819, "4400 Central Way", "https://www.iccu.com/", true),
-    new Location("DL Evans Bank", "DL Evans Bank", 42.907624, -112.465672, "4080 Yellowstone Ave", "https://www.dlevans.com/"),
-    new Location("Wells Fargo", "Wells Fargo", 42.909370, -112.466802, "4195 Yellowstone Ave", "https://www.wellsfargo.com/"),
-    new Location("Idaho Central Credit Union - Chubbuck Branch", "Idaho Central Credit Union - Chubbuck Branch", 42.915882, -112.466823, "4537 Yellowstone Ave", "https://www.iccu.com/", true),
-    new Location("Mountain America Credit Union", "Mountain America Credit Union", 42.911453, -112.464667,  "152 Bullock St", "https://www.macu.com", true),
-    new Location("Idaho State University Credit Union", "Idaho State University Credit Union", 42.9227200, -112.4658335, "4914 Yellowstone Ave", "https://www.isucu.org/", true),
-    new Location("Advantage Plus Credit Union", "Advantage Plus Credit Union", 42.920881, -112.468284, "150 W Chubbuck Rd", "https://advantagepluscreditunion.com", true),
-    new Location("Pocatello Railroad Federal Credit", "Pocatello Railroad Federal Credit", 42.918957, -112.465726, "4708 Yellowstone Ave", "https://railswestcu.org", true)
+    //var location = function (koTitle, title,latitude,longitude,streetAddress,website a.k.a facebook page)
+    new Location("CoHo Coffee", "CoHo Coffee", 42.8598574,  -112.4373139, "904 S 4th Ave", "https://www.facebook.com/Co.Ho.TheSmartBar/?fref=ts"),
+    new Location("College Market Coffeehouse", "College Market Coffeehouse", 42.864709, -112.4357415, "604 S 8th Ave", "https://www.facebook.com/The-College-Market-156572444544283/?fref=ts"),
+    new Location("Starbucks", "Starbucks", 42.8624186, -112.4430602, "330 E Benton St", "https://www.facebook.com/Starbucks-134135709964883/?fref=ts"),
+    new Location("Cafe Tuscano", "Cafe Tuscano", 42.8768060,-112.4214400, "2231 E Center St", "https://www.facebook.com/cafetuscanoinpocatello/?fref=ts"),
+    new Location("Rocky Mountain Ministries", "Rocky Mountain Ministries", 42.8890690,-112.4369930,  "845 Hyde Ave", "https://www.facebook.com/RockyMountainMin/?fref=ts"),
+    new Location("Double Shot Coffee Roasters & Donuts", "Double Shot Coffee Roasters & Donuts", 42.8873730,-112.4495730, "215 E Cedar St", "https://www.facebook.com/Double-Shot-Coffee-Roasters-Donuts-Hut-on-Jefferson-1046677665403362/?fref=nf"),
+    new Location("Java Express", "Java Express", 42.8871620,-112.4530680, "675 Yellowstone Ave", "https://www.facebook.com/pages/Java-Express/977154642305992")
 ]);
 
 //display or hide list
@@ -90,7 +89,7 @@ function makeMarkers(filtered) {
 
   google.maps.event.addListener(banks_allMarkers[i], "click", (function(banks_allMarkers, i) {
     return function() {
-      contentString = "<h3>" + filtered[i].title + "<h3>" + "<h4>" + filtered[i].streetAddress + "</h4>" + "<a href=" + "\"" +filtered[i].website + "\"" + "target=" + "\"" + "_blank" + "\"" + ">" + filtered[i].website + "</a>";
+      contentString = "<h3>" + filtered[i].title + "</h3>" + "<h3>" + filtered[i].streetAddress + "</h3>" + "<h4>" + "facebook:" + "</h4>" + "<a href=" + "\"" +filtered[i].website + "\"" + "target=" + "\"" + "_blank" + "\"" + ">" + filtered[i].website + "</a>";
       infowindow.setContent(contentString);
       infowindow.open(map, banks_allMarkers[i]);
 
@@ -111,8 +110,8 @@ function makeMarkers(filtered) {
   function initMap() {
 
     var mapOptions = {
-      zoom: 15,
-      center: {lat: 42.91255, lng: -112.466269},
+      zoom: 14,
+      center: {lat: 42.8768475, lng: -112.4507077},
       disableDefaultUI: true,
       zoomControl: true,
       panControl: true,
